8b72356a0e4fc6adc1826f3310c74051b96749a8
Nester <alechebil@mail.ru>
2025-04-11 14:30:11 +0300

    Create project with checkstyle

---
+++ src/main/java/ru/tasks/Task1.java
@@ -0,0 +1,18 @@
+package main.java.ru.tasks;
+
+public class Task1 {
+
+    private final String myVariable1;
+    private final String myVariable2;
+
+    private String newFieldForTask;
+
+    public Task1(String myVariable1, String myVariable2) {
+        this.myVariable1 = myVariable1;
+        this.myVariable2 = myVariable2;
+    }
+
+    public void doSomething() {
+        System.out.println(myVariable1 + myVariable2);
+    }
+}
